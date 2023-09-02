51ed3f06b095a183dadef9eee72b52828c7dac60
skfeat177 <134216016+skfeat177@users.noreply.github.com>
2023-09-02 14:47:16 +0530

    Update Files.js

--- src/pages/Files.js
+++ src/pages/Files.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
@@ -17,28 +17,65 @@ import CodeOutlinedIcon from '@mui/icons-material/CodeOutlined';
 import PhpIcon from '@mui/icons-material/Php';
 import FolderZipOutlinedIcon from '@mui/icons-material/FolderZipOutlined';
 import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
-import { Box, Skeleton,TextField } from '@mui/material';
-import Snackbar from '@mui/material/Snackbar';
+import { Box, Skeleton, Snackbar,CircularProgress,Alert } from '@mui/material';
 import MuiAlert from '@mui/material/Alert';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 function Files() {
   const [fileData, setFileData] = useState([]);
   const [snackbarOpen, setSnackbarOpen] = useState(false);
-  const [loading, setLoading] = useState(true);
   const [snackbarMessage, setSnackbarMessage] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+
+  const containerRef = useRef(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`https://quick-share-cors.vercel.app/getlimitedfiles?count=5&page=${page}`);
+      if (response.ok) {
+        const responseData = await response.json();
+        if (page === 1) {
+          // If it's the first page, replace the existing data
+          setFileData(responseData);
+        } else {
+          // If it's not the first page, append the new data
+          setFileData(prevData => [...prevData, ...responseData]);
+        }
+      } else {
+        // Handle other response status codes as needed
+        if (response.status === 404) {
+          setHasMore(false);
+        }
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setLoading(false); // Data is loaded or if there's an error, set loading to false
+    }
+  };
+  
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1); // Load the next page when the "Load More" button is clicked
+  };
 
   useEffect(() => {
-    // Fetch data using fetch API
-    fetch('https://quick-share-cors.vercel.app/getallfiles')
-      .then(response => response.json())
-      .then(data => {
-        setFileData((data.files).reverse());
-        setLoading(false)
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
+    fetchData(); // Fetch data when the component mounts or when the page changes
+  }, [page]);
+  
+  
+  const handleScroll = () => {
+    if (
+      containerRef.current &&
+      window.innerHeight + window.scrollY >= containerRef.current.offsetHeight - 100 &&
+      fileData.length > 0
+    ) {
+      // Load more data when the user is near the bottom
+      setPage(prevPage => prevPage + 1);
+    }
+  };
+
 
   const handleDeleteClick = (fileId) => {
     fetch(`https://quick-share-cors.vercel.app/deletefile?id=${fileId}`, {
@@ -46,7 +83,7 @@ function Files() {
     })
     .then(response => response.json())
     .then(data => {
-      setSnackbarMessage('File deleted successfully');
+      setSnackbarMessage(data.message);
       setSnackbarOpen(true);
       // Update file list by filtering out the deleted file
       setFileData(prevFileData => prevFileData.filter(file => file._id !== fileId));
@@ -98,11 +135,100 @@ function Files() {
   };
 
   return (
-    <div style={{ marginInline: '10px', marginTop: '90px', marginBottom: '90px' }}>
-    {loading ? (
-      // Display skeleton loading components while data is loading
-      Array.from({ length: 5 }).map((_, index) => (
-        <Card key={index} sx={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginBottom: '10px' }} elevation={3}>
+    <div
+      ref={containerRef}
+      style={{
+        marginInline: 'auto',
+        marginTop: '90px',
+        marginBottom: '90px',
+        display: 'flex',
+        justifyContent: 'flex-start',
+        flexWrap: 'wrap',
+        overflowX: 'hidden', // Add this to prevent horizontal overflow
+      }}
+      onScroll={handleScroll}
+    >
+      {loading ? (
+        <SkeletonLoader/>
+      ) : (
+        <Box sx={{width:'100%',height:'100%'}}>
+<InfiniteScroll
+  dataLength={fileData.length}
+  next={() => setPage((prevPage) => prevPage + 1)}
+  hasMore={hasMore}
+  loader={
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80px'}}>
+      <CircularProgress thickness={6} size={35}/>     <p style={{ fontSize:25,marginLeft:'10px',color:'grey' }}>
+      <b>Loading..</b>
+    </p>
+    </div>
+  }
+  endMessage={
+    <p style={{ textAlign: 'center',fontSize:20 }}>
+      <b>No more data to load</b>
+    </p>
+  }
+  style={{display:'flex',justifyContent:'center' ,flexDirection:'column',marginInline:'auto'}}
+>
+{fileData.map((file) => (
+  <div style={{width:"100%",marginInline:'auto'}}>
+    <Card key={file._id} sx={{ width: '98%', display: 'flex', alignItems: 'center', marginBottom: '10px' ,justfySelf:'center',marginLeft:'13px'}} elevation={3} >
+      <CardContent sx={{ width: '100%' }}>
+        <Typography variant="body1" sx={{ fontWeight: 'bold', color: 'black', width: '100%' }}>
+          {file.fileDescription}
+        </Typography>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          {getIconForFileType(file.fileType)}
+          <Box sx={{ marginLeft: 2 }}>
+            <Typography variant="body2" sx={{ color: 'grey' }}>{new Date(file.uploadedDate).toLocaleString('en-IN', options)}</Typography>
+            <Typography variant="body2" sx={{ color: 'grey' }}>{(file.fileSize / 1024).toFixed(2)} KB</Typography>
+          </Box>
+        </Box>
+      </CardContent>
+      <CardActions>
+        {/* Delete Button */}
+        <IconButton color="error" onClick={() => handleDeleteClick(file._id)}>
+          <DeleteOutlinedIcon />
+        </IconButton>
+
+        {/* Download Button */}
+        <a href={file.fileUrl} rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+          <IconButton color="primary">
+            <GetAppOutlinedIcon />
+          </IconButton>
+        </a>
+      </CardActions>
+    </Card>
+    </div>
+  ))}
+</InfiniteScroll>
+</Box>
+
+      )}
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={1000}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        onClose={() => setSnackbarOpen(false)}
+        sx={{ marginBottom: 7 }}
+      >
+        <Alert
+          elevation={6}
+          variant="filled"
+          onClose={() => setSnackbarOpen(false)}
+          severity="success"
+        >
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+}
+
+function SkeletonLoader() {
+  return (
+    Array.from({ length: 5 }).map((_, index) => (
+        <Card key={index} sx={{ width: '98%', display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginBottom: '10px',marginLeft:'14px' }} elevation={3}>
           <CardContent>
             <Typography variant="body1">
               <Skeleton width={'100%'} />
@@ -129,59 +255,6 @@ function Files() {
           </CardActions>
         </Card>
       ))
-    ) : (
-      // Display content when data is available
-      fileData.map(file => (
-        <Card key={file._id} sx={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginBottom: '10px' }} elevation={3}>
-          <CardContent>
-            <Typography variant="body1" sx={{ fontWeight: 'bold', color: 'black', width: '100%' }}>
-              {file.fileDescription}
-            </Typography>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              {getIconForFileType(file.fileType)}
-              <Box sx={{ marginLeft: 2 }}>
-                <Typography variant="body2" sx={{ color: 'grey' }}>{new Date(file.uploadedDate).toLocaleString('en-IN', options)}</Typography>
-                <Typography variant="body2" sx={{ color: 'grey' }}>{(file.fileSize / 1024).toFixed(2)} KB</Typography>
-              </Box>
-            </Box>
-          </CardContent>
-          <CardActions>
-            {/* Delete Button */}
-            <IconButton color="error" onClick={() => handleDeleteClick(file._id)}>
-              <DeleteOutlinedIcon />
-            </IconButton>
-
-            {/* Download Button */}
-            <a href={file.fileUrl} rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
-            <IconButton
-              color="primary"
-              
-            > 
-              <GetAppOutlinedIcon />
-            </IconButton>
-            </a>
-          </CardActions>
-        </Card>
-      ))
-    )}
-    {/* Snackbar for showing success message */}
-    <Snackbar
-      open={snackbarOpen}
-      autoHideDuration={1000}
-      anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-      onClose={() => setSnackbarOpen(false)}
-      sx={{marginBottom:7}}
-    >
-      <MuiAlert
-        elevation={6}
-        variant="filled"
-        onClose={() => setSnackbarOpen(false)}
-        severity="success"
-      >
-        {snackbarMessage}
-      </MuiAlert>
-    </Snackbar>
-  </div>
   );
 }
 

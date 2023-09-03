cda9c75419b5307b754061d28bfddc4e97ac32ad
skfeat177 <134216016+skfeat177@users.noreply.github.com>
2023-09-03 21:57:59 +0530

    alert fix

--- src/pages/UploadLink.js
+++ src/pages/UploadLink.js
@@ -96,7 +96,7 @@ function UploadText() {
           </Box>
         </CardContent>
       </Card>
-      <Snackbar anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }} open={alertOpen} autoHideDuration={500} onClose={() => setAlertOpen(false)}>
+      <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'center' }} open={alertOpen} autoHideDuration={500} onClose={() => setAlertOpen(false)}>
       <MuiAlert severity={alertSeverity}
          elevation={6}
           variant="filled" 
